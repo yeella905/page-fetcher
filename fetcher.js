0e2529954e0570e6ed11fb99bcdea7306096a4cb
yeella905 <yeella905@gmail.com>
2025-01-04 19:02:51 -0500

    version 2

--- fetcher.js
+++ fetcher.js
@@ -1,9 +1,19 @@
-const fs = require('node:fs');
-const content = 'Some content!';
-fs.writeFile('http://www.example.edu/ ./index.html', content, err => {
-  if (err) {
-    console.error(err);
-  } else {
-   console.log("Downloaded and saved 3261 bytes to ./index.html");
-  }
+const needle = require('needle');
+const fs = require('fs');
+
+// URL to fetch the content
+const url = 'http://www.example.edu/';
+
+needle.get(url, (err, res) => {
+    if (err) throw err;
+
+  // Writing the fetched data to the local file
+  fs.writeFile('./index.html', res.body, error => {
+    if (error) {
+      console.error('Error writing to file:', error);
+    } else {
+      const fileSizeInBytes = Buffer.byteLength(res.body);
+      console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ./index.html`);
+    }
+  });
 });
